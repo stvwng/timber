9961f6ba3ee219d22bb21d62203efa9402011d2f
stvwng <122651196+stvwng@users.noreply.github.com>
2024-04-07 20:03:34 -0400

    Add tree, bee and clouds

--- timber.cpp
+++ timber.cpp
@@ -25,6 +25,41 @@ int main() {
     // Set the spriteBackground to cover the screen
     spriteBackground.setPosition(0,0);
 
+    // Make a tree sprite
+    Texture textureTree;
+    textureTree.loadFromFile("graphics/tree.png");
+    Sprite spriteTree;
+    spriteTree.setTexture(textureTree);
+    spriteTree.setPosition(810, 0);
+
+    // Prepare the bee
+    Texture textureBee;
+    textureBee.loadFromFile("graphics/bee.png");
+    Sprite spriteBee;
+    spriteBee.setTexture(textureBee);
+    spriteBee.setPosition(0, 800);
+    bool beeActive = false;
+    float beeSpeed = 0.0f;
+
+    // Make cloud sprites
+    Texture textureCloud;
+    textureCloud.loadFromFile("graphics/cloud.png");
+    Sprite spriteCloud1;
+    Sprite spriteCloud2;
+    Sprite spriteCloud3;
+    spriteCloud1.setTexture(textureCloud);
+    spriteCloud2.setTexture(textureCloud);
+    spriteCloud3.setTexture(textureCloud);
+    spriteCloud1.setPosition(0,0);
+    spriteCloud2.setPosition(0,250);
+    spriteCloud3.setPosition(0, 500);
+    bool cloud1Active = false;
+    bool cloud2Active = false;
+    bool cloud3Active = false;
+    float cloud1Speed = 0.0f;
+    float cloud2Speed = 0.0f;
+    float cloud3Speed = 0.0f;
+
     while (window.isOpen())
     {
         // Handle player input
@@ -40,6 +75,17 @@ int main() {
 
         // Draw game scene
         window.draw(spriteBackground);
+
+        // Draw clouds
+        window.draw(spriteCloud1);
+        window.draw(spriteCloud2);
+        window.draw(spriteCloud3);
+
+        // Draw tree
+        window.draw(spriteTree);
+
+        // Draw bee
+        window.draw(spriteBee);
 
         // Show everything we drew
         window.display();
